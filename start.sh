9b2ef38cc8985191dbdd0d6f0c4f462765bced6f
Admins <bmt2000gg@gmail.com>
2023-10-31 09:53:57 +0700

    add

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gunicorn -w 4 -b 0.0.0.0:5000 api_request:app &
-gunicorn -w 1 -b 0.0.0.0:8000 api_scheduler:app
+gunicorn -w 4 -b 0.0.0.0:5000 api_request.app:app &
+gunicorn -w 1 -b 0.0.0.0:8000 api_scheduler.app:app
