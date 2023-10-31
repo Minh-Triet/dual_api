74ba9ad18ab6411921adcefc5fe426a7d6d508e7
Admins <bmt2000gg@gmail.com>
2023-10-31 10:54:19 +0700

    add

--- api_request/development_config.py
+++ api_request/development_config.py
@@ -1,16 +1,11 @@
 import logging
 
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
 check_scheduler = []
 logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
-
-jobstores = {
-    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
-}
 
 executors = {
     'default': ThreadPoolExecutor(1),
@@ -21,5 +16,5 @@ job_defaults = {
     'coalesce': False,
     'max_instances': 1
 }
-scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors,
+scheduler = BackgroundScheduler(executors=executors,
                                 job_defaults=job_defaults, timezone='Asia/ho_chi_minh')
